2f30fc4930d77974f35995760897b238b641a229
Owen Ou <jingweno@gmail.com>
2020-09-03 22:12:39 -0700

    Tweak config

--- lib/game/game_config.dart
+++ lib/game/game_config.dart
@@ -7,7 +7,6 @@ class GameConfig {
   static double gameoverClearTime = 750.0;
   static double gapCoefficient = 0.6;
   static double gravity = 10;
-  static double initialJumpVelocity = 12.0;
   static double invertFadeDuration = 12000.0;
   static double invertDistance = 700.0;
   static double maxBlinkCount = 3.0;
@@ -15,7 +14,6 @@ class GameConfig {
   static double maxObstacleLength = 3.0;
   static int maxObstacleDuplication = 2;
   static double maxSpeed = 50.0;
-  static double minJumpHeight = 35.0;
-  static double speed = 12;
+  static double speed = 10;
   static double speedDropCoefficient = 3.0;
 }
