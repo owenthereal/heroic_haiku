433745aa585f36aea4e2af9db1eabb7cb796b2f7
Owen Ou <jingweno@gmail.com>
2020-09-05 11:07:44 -0700

    Use mp3 instead ogg because Safari doesn't have support

--- lib/game/audio/audio.dart
+++ lib/game/audio/audio.dart
@@ -3,23 +3,23 @@ import 'package:flame/flame.dart';
 class Audio {
   static Future<void> load() async {
     Flame.bgm.initialize();
-    Flame.bgm.loadAll(["bgm.ogg"]);
-    Flame.audio.loadAll(["jump.ogg", "start.ogg", "crashed.ogg"]);
+    Flame.bgm.loadAll(["bgm.mp3"]);
+    Flame.audio.loadAll(["jump.mp3", "start.mp3", "crashed.mp3"]);
   }
 
   static void playJump() {
-    Flame.audio.play("jump.ogg");
+    Flame.audio.play("jump.mp3");
   }
 
   static void playCrashed() {
-    Flame.audio.play("crashed.ogg");
+    Flame.audio.play("crashed.mp3");
   }
 
   static void loopBGM() {
-    Flame.bgm.play("bgm.ogg");
+    Flame.bgm.play("bgm.mp3");
   }
 
   static void playNewStart() {
-    Flame.audio.play("start.ogg", volume: 0.6);
+    Flame.audio.play("start.mp3", volume: 0.6);
   }
 }
